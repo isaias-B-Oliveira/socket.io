ee45309158e37deb4f514fda3a292323e71da2ae
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2024-09-11 19:51:59 -0300

    feat: model updatesmg in sql

--- backend/src/models/messageModel.js
+++ backend/src/models/messageModel.js
@@ -1,19 +1,18 @@
-const connection = require('./connection');
-
-const addMessage = async (messageData) => {
-  const { message, userId,  channelId} = messageData;
-  const sql = 'INSERT INTO chatTime.menssage (message, user_id, chanel_id) VALUE (?, ?, ?)';
-  await connection.execute(sql, [message, userId, channelId])
-};
+const connection = require("./connection");
 
 const getMessageByForeignKey = async (id) => {
- const sql = `
+    const sql = `
 SELECT message, data_time, C.name as UserName, B.name as channelName, description, last_name 
 FROM chatTime.menssage as A INNER JOIN chatTime.chanel as B
 INNER JOIN chatTime.user as C on A.chanel_id = B.id AND A.chanel_id=?;
 `;
- const [msgs] = await connection.execute(sql, [id]);
- return msgs;
+    const [msgs] = await connection.execute(sql, [id]);
+    return msgs;
 };
 
-module.exports = { addMessage, getMessageByForeignKey };
+const updateMessage = async (messageId, newMessage) => {
+    const sql = "UPDATE chatTime.menssage SET message = ? WHERE id = ?";
+    await connection.execute(sql, [newMessage, messageId]);
+};
+
+module.exports = { getMessageByForeignKey, updateMessage };
